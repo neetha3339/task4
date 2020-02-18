1b634966e7f50e46f34e8e741daec2e427a6e152
neetha3339 <60148569+neetha3339@users.noreply.github.com>
2020-02-18 20:26:03 +0530

    Add files via upload

---
+++ HouseEstimationCost.java
@@ -0,0 +1,17 @@
+package task4;
+
+public class HouseEstimationCost {
+	public float fullyAutomatedHome(float total_area_of_house)
+	{
+		float amount;
+		amount=2500*total_area_of_house;
+		return amount;
+	}
+	//Method to calculate the estimated house construction cost for a non-automated house
+	public float Home(float total_area_of_house,int cost_per_square_feet)
+	{
+		float amount;
+		amount=cost_per_square_feet*total_area_of_house;
+		return amount;
+	}
+}
